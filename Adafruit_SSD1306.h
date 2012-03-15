18ec5c14d317eff96208bcadce2c23cadfce1b8e
Ladyada <limor@ladyada.net>
2012-03-15 15:03:21 -0400

    headers

--- Adafruit_SSD1306.h
+++ Adafruit_SSD1306.h
@@ -1,3 +1,22 @@
+/*********************************************************************
+This is a library for our Monochrome OLEDs based on SSD1306 drivers
+
+  Pick one up today in the adafruit shop!
+  ------> http://www.adafruit.com/category/63_98
+
+These displays use SPI to communicate, 4 or 5 pins are required to  
+interface
+
+Adafruit invests time and resources providing this open source code, 
+please support Adafruit and open-source hardware by purchasing 
+products from Adafruit!
+
+Written by Limor Fried/Ladyada  for Adafruit Industries.  
+BSD license, check license.txt for more information
+All text above, and the splash screen must be included in any redistribution
+*********************************************************************/
+
+
 #if ARDUINO >= 100
  #include "Arduino.h"
 #else
