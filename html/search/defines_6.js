7a316a1e2291106f2225998c3195e656a492bee3
Travis CI <travis@travis-ci.org>
2018-11-17 18:07:45 +0000

    Deploy code docs to GitHub Pages Travis build: 8
    Commit: 1973f0001f28ee48fe3d501b0adb8a41ab62f508

--- html/search/defines_6.js
+++ html/search/defines_6.js
@@ -2,6 +2,5 @@ var searchData=
 [
   ['white',['WHITE',['../_adafruit___s_s_d1306_8h.html#a87b537f5fa5c109d3c05c13d6b18f382',1,'Adafruit_SSD1306.h']]],
   ['wire_5fmax',['WIRE_MAX',['../_adafruit___s_s_d1306_8cpp.html#a9cc15bdad03e0e0b1eb2ca529c8900e9',1,'Adafruit_SSD1306.cpp']]],
-  ['wire_5fwrite',['WIRE_WRITE',['../_adafruit___s_s_d1306_8cpp.html#aa651e488d89d384a0c41ca3ff06e44f0',1,'Adafruit_SSD1306.cpp']]],
-  ['wireclk',['WIRECLK',['../_adafruit___s_s_d1306_8cpp.html#a438ade9d9a487c1062efbe559231314f',1,'Adafruit_SSD1306.cpp']]]
+  ['wire_5fwrite',['WIRE_WRITE',['../_adafruit___s_s_d1306_8cpp.html#aa651e488d89d384a0c41ca3ff06e44f0',1,'Adafruit_SSD1306.cpp']]]
 ];
