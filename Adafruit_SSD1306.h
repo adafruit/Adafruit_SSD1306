78701a4d21316dc784d8f956d5f59a8e4e75cc9a
Martino Facchin <m.facchin@arduino.cc>
2019-10-23 14:41:38 +0200

    Don't define HAVE_PORTREG for mbed target
    Fixes https://github.com/arduino/Arduino/issues/9345

--- Adafruit_SSD1306.h
+++ Adafruit_SSD1306.h
@@ -48,7 +48,7 @@
   typedef volatile RwReg    PortReg;
   typedef uint32_t          PortMask;
   #define HAVE_PORTREG
-#elif defined(__arm__) || defined(ARDUINO_FEATHER52)
+#elif (defined(__arm__) || defined(ARDUINO_FEATHER52)) && !defined(ARDUINO_ARCH_MBED)
   typedef volatile uint32_t PortReg;
   typedef uint32_t          PortMask;
   #define HAVE_PORTREG
