749c8e632b8be4875f8582ca032343ec1f47a142
Chilkings <chilkings@foxmail.com>
2020-06-27 20:33:25 +0800

    change the address of 128x64
    The address of 128x64 should be 0X3C

--- examples/ssd1306_128x64_i2c/ssd1306_128x64_i2c.ino
+++ examples/ssd1306_128x64_i2c/ssd1306_128x64_i2c.ino
@@ -56,7 +56,7 @@ void setup() {
   Serial.begin(9600);
 
   // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
-  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3D)) { // Address 0x3D for 128x64
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
     Serial.println(F("SSD1306 allocation failed"));
     for(;;); // Don't proceed, loop forever
   }
