fff9954417050f78c67a1dbbc196ea2f930fe469
caternuson <caternuson@gmail.com>
2022-03-31 16:29:43 -0700

    example update for feather rp2040

--- examples/OLED_featherwing/OLED_featherwing.ino
+++ examples/OLED_featherwing/OLED_featherwing.ino
@@ -3,34 +3,46 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
-Adafruit_SSD1306 display = Adafruit_SSD1306(128, 32, &Wire);
-
-// OLED FeatherWing buttons map to different pins depending on board:
+// OLED FeatherWing buttons map to different pins depending on board.
+// The I2C (Wire) bus may also be different.
 #if defined(ESP8266)
   #define BUTTON_A  0
   #define BUTTON_B 16
   #define BUTTON_C  2
+  #define WIRE Wire
 #elif defined(ESP32)
   #define BUTTON_A 15
   #define BUTTON_B 32
   #define BUTTON_C 14
+  #define WIRE Wire
 #elif defined(ARDUINO_STM32_FEATHER)
   #define BUTTON_A PA15
   #define BUTTON_B PC7
   #define BUTTON_C PC5
+  #define WIRE Wire
 #elif defined(TEENSYDUINO)
   #define BUTTON_A  4
   #define BUTTON_B  3
   #define BUTTON_C  8
+  #define WIRE Wire
 #elif defined(ARDUINO_FEATHER52832)
   #define BUTTON_A 31
   #define BUTTON_B 30
   #define BUTTON_C 27
+  #define WIRE Wire
+#elif defined(ARDUINO_ADAFRUIT_FEATHER_RP2040)
+  #define BUTTON_A  9
+  #define BUTTON_B  8
+  #define BUTTON_C  7
+  #define WIRE Wire1
 #else // 32u4, M0, M4, nrf52840 and 328p
   #define BUTTON_A  9
   #define BUTTON_B  6
   #define BUTTON_C  5
+  #define WIRE Wire
 #endif
+
+Adafruit_SSD1306 display = Adafruit_SSD1306(128, 32, &WIRE);
 
 void setup() {
   Serial.begin(9600);
