dcdc79b2992ab254c290c0500ef177fe897c9658
Meco Man <920369182@qq.com>
2023-06-01 13:21:53 -0400

    add ARDUINO_ARCH_RTTHREAD to support RT-Thread

--- Adafruit_SSD1306.cpp
+++ Adafruit_SSD1306.cpp
@@ -36,7 +36,7 @@
  *
  */
 
-#ifdef __AVR__
+#if defined(__AVR__) || defined(ARDUINO_ARCH_RTTHREAD)
 #include <avr/pgmspace.h>
 #elif defined(ESP8266) || defined(ESP32) || defined(ARDUINO_ARCH_RP2040)
 #include <pgmspace.h>
